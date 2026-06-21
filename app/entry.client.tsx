c7c728ad1062e021dc35ddef3142d15ca296e060
Bryan Ross <git@rossipedia.com>
2026-06-21 10:09:01 -0600

    extract initialContext

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,31 +1,32 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { startTransition, StrictMode } from "react";
-import { hydrateRoot } from "react-dom/client";
-import { RouterContextProvider } from "react-router";
-import { HydratedRouter } from "react-router/dom";
-import { QueryClientRRContext } from "./query-client-context.client";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { startTransition, StrictMode } from 'react'
+import { hydrateRoot } from 'react-dom/client'
+import { RouterContextProvider } from 'react-router'
+import { HydratedRouter } from 'react-router/dom'
+
+import { QueryClientRRContext } from './query-client-context.client'
+
+const queryClient = new QueryClient({
+  // ... options
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
+const initialContext = new Map([[QueryClientRRContext, queryClient]])
 
 startTransition(() => {
-  const queryClient = new QueryClient({
-    // ... options
-    defaultOptions: {
-      queries: {
-        staleTime: 1000 * 60 * 5,
-        refetchOnWindowFocus: false,
-      },
-    },
-  });
-
   hydrateRoot(
     document,
     <StrictMode>
       <QueryClientProvider client={queryClient}>
         <HydratedRouter
-          getContext={() =>
-            new RouterContextProvider(new Map([[QueryClientRRContext, queryClient]]))
-          }
+          getContext={() => new RouterContextProvider(initialContext)}
         />
       </QueryClientProvider>
     </StrictMode>,
-  );
-});
+  )
+})
